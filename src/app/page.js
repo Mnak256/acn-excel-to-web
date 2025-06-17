d71a070a4bfd19a7f07d72e2a9eeccd2b89b28b4
Mnak256 <dutta.mainak256@gmail.com>
2025-06-17 11:36:59 +0530

    add routes, add header

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
+import Link from "next/link";
 import styles from "./page.module.css";
+import testDataStyles from "@/app/test-data/page.module.css";
+import routes from "@/components/utils/routes";
 
 export default function Home() {
   return (
-    <>
+    <div className={testDataStyles.field}>
       <div>Main content</div>
-    </>
+      <Link href={routes.testData}>Input Form</Link>
+    </div>
   );
 }
