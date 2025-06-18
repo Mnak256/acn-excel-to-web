ac438a1a0037eb972b0787135a15b3de7e4099cb
Mnak256 <dutta.mainak256@gmail.com>
2025-06-19 01:32:14 +0530

    Show Vehicle Type and VIN only if Line of Business is Auto

--- src/components/TestData/CustomDatePicker.js
+++ src/components/TestData/CustomDatePicker.js
@@ -1,13 +1,24 @@
 import { DatePicker } from "antd";
 import styles from "@/app/test-data/page.module.css";
+import dayjs from "dayjs";
 
-const CustomDatePicker = ({ id, name, label }) => {
+const CustomDatePicker = ({ id, name, label, value, onChange }) => {
+  const customOnChange = (_, dateString) => {
+    onChange({ target: { value: dateString } });
+  };
+
   return (
     <>
       <label htmlFor={id} className={styles.label}>
         {label}
       </label>
-      <DatePicker id={id} name={name} style={{ border: "1px solid #636363" }} />
+      <DatePicker
+        id={id}
+        name={name}
+        style={{ border: "1px solid #636363" }}
+        value={dayjs(value)}
+        onChange={customOnChange}
+      />
     </>
   );
 };
--- src/components/TestData/UserTypeDropdown.js
+++ src/components/TestData/UserTypeDropdown.js
@@ -1,12 +1,18 @@
 import styles from "@/app/test-data/page.module.css";
 
-const UserTypeDropdown = ({ id, label }) => {
+const UserTypeDropdown = ({ id, label, value, onChange }) => {
   return (
     <>
       <label htmlFor={id} className={styles.label}>
         {label}
       </label>
-      <select id={id} name={id} className={styles.select}>
+      <select
+        id={id}
+        name={id}
+        className={styles.select}
+        value={value}
+        onChange={onChange}
+      >
         <option value="EMPLOYEE">EMPLOYEE</option>
         <option value="AGENT">AGENT</option>
         <option value="MGA">MGA</option>
